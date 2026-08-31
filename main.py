1cc3d0588e4d0c6f912e945242ac822af0633895
azizayedi091-create <azizayedi091@gmail.com>
2026-08-31 04:33:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Mess
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # ==================== CONFIG & DATABASE ====================
-TOKEN = "YOUR_BOT_TOKEN_HERE"
-ADMIN_ID = 123456789  # ⚠️ حط الـ Telegram ID متاعك هنا
+TOKEN = "8671890016:AAE8BHBQh-90oqVvtom2UlMLnCLHl19DLkY"
+ADMIN_ID = 6605879863  # ⚠️ حط الـ Telegram ID متاعك هنا
 
 # مخزن المفاتيح (Stock الديناميكي)
 STOCK_DB = {}
